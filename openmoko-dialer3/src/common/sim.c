e1c9fbc97435643c52f542e55ad4fae9cc63080d
ainulindale <ainulindale@a7dca5fb-7094-41a2-a069-28cb1271ccbd>
2008-07-15 22:03:08 +0000

    Stop wurp's whining
    git-svn-id: svn://daria.forty-two.fr/shr/trunk@27 a7dca5fb-7094-41a2-a069-28cb1271ccbd

--- openmoko-dialer3/src/common/sim.c
+++ openmoko-dialer3/src/common/sim.c
@@ -18,20 +18,24 @@ int get_authentication_state() {
 
 	char *status = NULL;
 	GError *error = NULL;
+	int result = -1;
 	if (!dbus_g_proxy_call (simBus, "GetAuthStatus", &error,
 				G_TYPE_INVALID,
 				G_TYPE_STRING, &status, G_TYPE_INVALID))
 		lose_gerror ("Failed to complete GetAuthStatus", error);
-
+	
 	if(strcmp(status,DBUS_SIM_READY))
-		return SIM_READY;
+		result = SIM_READY;
 	else if(strcmp(status,DBUS_SIM_PIN_REQUIRED))
-		return SIM_PIN_REQUIRED;
+		result = SIM_PIN_REQUIRED;
 	else if(strcmp(status,DBUS_SIM_PUK_REQUIRED))
-		return SIM_PUK_REQUIRED;
+		result = SIM_PUK_REQUIRED;
 	else if(strcmp(status,DBUS_SIM_PIN2_REQUIRED))
-		return SIM_PIN2_REQUIRED;
-	return SIM_PUK2_REQUIRED;
+		result = SIM_PIN2_REQUIRED;
+	else
+		result = SIM_PUK2_REQUIRED;
+	free(status);
+	return result;
 }
 
 
