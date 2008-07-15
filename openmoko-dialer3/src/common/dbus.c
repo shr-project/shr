ed6e89cd5e3159b7e318b6449649fe84440d7981
ainulindale <ainulindale@a7dca5fb-7094-41a2-a069-28cb1271ccbd>
2008-07-15 20:43:45 +0000

    First edible draft.
    git-svn-id: svn://daria.forty-two.fr/shr/trunk@23 a7dca5fb-7094-41a2-a069-28cb1271ccbd

--- openmoko-dialer3/src/common/dbus.c
+++ openmoko-dialer3/src/common/dbus.c
@@ -5,8 +5,82 @@
 
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-bindings.h>
+#include "dbus.h"
+
+static void lose (const char *str, ...)
+{
+	va_list args;
+
+	va_start (args, str);
+
+	vfprintf (stderr, str, args);
+	fputc ('\n', stderr);
+
+	va_end (args);
+
+	exit (1);
+}
+
+static void lose_gerror (const char *prefix, GError *error)
+{
+	lose ("%s: %s", prefix, error->message);
+}
+
+static void network_status_handler (DBusGProxy *proxy, const  GHashTable ** status, gpointer user_data)
+{
+  printf ("Received network status");
+}
+
+static void network_signal_strength_handler (DBusGProxy *proxy, const int signal_strength, gpointer user_data)
+{
+  printf ("Received network signal strength");
+}
+
+static void sim_auth_status_handler (DBusGProxy *proxy, const char *status, gpointer user_data)
+{
+  printf ("Received sim auth status");
+}
+
+static void call_status_handler (DBusGProxy *proxy, const int id, const char *status, const GHashTable ** properties, gpointer user_data)
+{
+  printf ("Received call status");
+}
+
+
 
 int main(int argc, char ** argv) {
+	DBusGConnection *bus;
+	DBusGProxy *networkBus, *simBus, *callBus;
+	GError *error = NULL;
+	GMainLoop *mainloop = NULL;
+	g_type_init ();
+
+	{
+		GLogLevelFlags fatal_mask;
+
+		fatal_mask = g_log_set_always_fatal (G_LOG_FATAL_MASK);
+		fatal_mask |= G_LOG_LEVEL_WARNING | G_LOG_LEVEL_CRITICAL;
+		g_log_set_always_fatal (fatal_mask);
+	}
+
+	bus = dbus_g_bus_get (DBUS_BUS_SYSTEM, &error);
+
+
+	if (!bus)
+		lose_gerror ("Couldn't connect to system bus", error);
+
+	networkBus = dbus_g_proxy_new_for_name (bus, NETWORKD_BUS, BUS_PATH, NETWORK_INTERFACE);
+	simBus = dbus_g_proxy_new_for_name (bus, GSMD_BUS, BUS_PATH, SIM_INTERFACE);
+	callBus = dbus_g_proxy_new_for_name (bus, GSMD_BUS, BUS_PATH, CALL_INTERFACE);
+
+  	mainloop = g_main_loop_new (NULL, FALSE);
+	
+
+	dbus_g_proxy_add_signal (networkBus, "Status", DBUS_TYPE_G_STRING_VARIANT_HASHTABLE, G_TYPE_INVALID);
+	dbus_g_proxy_connect_signal (networkBus, "Status", G_CALLBACK (network_status_handler),
+                                     NULL, NULL);
+
+	g_main_loop_run (mainloop);
 
 	exit(EXIT_SUCCESS);
 
--- openmoko-dialer3/src/common/dbus.h
+++ openmoko-dialer3/src/common/dbus.h
@@ -1,7 +1,14 @@
-#DEFINE NETWORKD_BUS		"org.freesmartphone.onetworkd"
-#DEFINE GSMD_BUS		"org.freesmartphone.ogsmd"
-#DEFINE BUS_PATH		"/org/freesmartphone/GSM/Device"
-#DEFINE CALL_INTERFACE		"org.freesmartphone.GSM.Call"
-#DEFINE NETWORK_INERFACE 	"org.freesmartphone.GSM.Network"
-#DEFINE	DEVICE_INTERFACE	"org.freesmartphone.GSM.Device"
+#ifndef _DBUS_H
+#define _DBUS_H
 
+#define NETWORKD_BUS		"org.freesmartphone.onetworkd"
+#define GSMD_BUS		"org.freesmartphone.ogsmd"
+#define BUS_PATH		"/org/freesmartphone/GSM/Device"
+#define CALL_INTERFACE		"org.freesmartphone.GSM.Call"
+#define SIM_INTERFACE		"org.freesmartphone.GSM.SIM"
+#define NETWORK_INTERFACE 	"org.freesmartphone.GSM.Network"
+#define	DEVICE_INTERFACE	"org.freesmartphone.GSM.Device"
+
+#define DBUS_TYPE_G_STRING_VARIANT_HASHTABLE (dbus_g_type_get_map ("GHashTable", G_TYPE_STRING, G_TYPE_VALUE))
+
+#endif
