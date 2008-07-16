6d716cd5e56fcd46af758b07ac88dc36f809d790
ainulindale <ainulindale@a7dca5fb-7094-41a2-a069-28cb1271ccbd>
2008-07-16 16:43:25 +0000

    Added copyrights.
    git-svn-id: svn://daria.forty-two.fr/shr/trunk@31 a7dca5fb-7094-41a2-a069-28cb1271ccbd

--- openmoko-dialer3/src/phonekitd/network.c
+++ openmoko-dialer3/src/phonekitd/network.c
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (C) 2008
+ *      Authors (alphabetical) :
+ *              Marc-Olivier Barre <marco@marcochapeau.org>
+ *              Julien Cassignol <ainulindale@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Public License as published by
+ *  the Free Software Foundation; version 2 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
