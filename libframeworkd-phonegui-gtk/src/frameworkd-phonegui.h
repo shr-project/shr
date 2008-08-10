a84e94c9736e5d7621591600e932fa9bfb3d42ab
andrease <andrease@a7dca5fb-7094-41a2-a069-28cb1271ccbd>
2008-08-10 12:43:46 +0000

    * Corrected CFLAG in .pc file * made stubs for phonegui_display_call_UI in frameworkd-phonegui.h
    git-svn-id: svn://daria.forty-two.fr/shr/trunk@212 a7dca5fb-7094-41a2-a069-28cb1271ccbd

--- libframeworkd-phonegui-gtk/src/frameworkd-phonegui.h
+++ libframeworkd-phonegui-gtk/src/frameworkd-phonegui.h
@@ -22,6 +22,16 @@ void phonegui_init(int argc, char** argv);
 void phonegui_display_pin_UI (const int initial_status);
 void phonegui_destroy_pin_UI ();
 
-//void phonegui_display_call_UI(const int id_call, const int status, const char *number);
-//void phonegui_destroy_call_UI(const int id_call);
+void phonegui_display_call_UI(const int id_call, const int status, const char *number);
+void phonegui_destroy_call_UI(const int id_call);
+
+void phonegui_display_call_UI(const int id_call, const int status, const char *number)
+{
+/* stub */
+}
+
+void phonegui_destroy_call_UI(const int id_call)
+{
+/* stub */
+}
 #endif
