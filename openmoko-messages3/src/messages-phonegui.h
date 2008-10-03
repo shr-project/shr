3516e4bf05a8d1bf2726ab2d4930082e8bdf5021
quickdev <quickdev@a7dca5fb-7094-41a2-a069-28cb1271ccbd>
2008-10-03 17:10:20 +0000

    Introduced phonegui library loading.
    git-svn-id: svn://daria.forty-two.fr/shr/trunk@248 a7dca5fb-7094-41a2-a069-28cb1271ccbd

---
+++ openmoko-messages3/src/messages-phonegui.h
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (C) 2008
+ *      Authors (alphabetical) :
+ *              quickdev
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Public License as published by
+ *  the Free Software Foundation; version 2 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ */
+
+#ifndef _MESSAGES_PHONEGUI_H
+#define _MESSAGES_PHONEGUI_H
+
+#include <frameworkd-glib/frameworkd-glib-phonegui.h>
+
+void (*phonegui_messages_launch)();
+
+void phonegui_connect();
+
+#endif
