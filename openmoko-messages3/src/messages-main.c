31b8f1b52463271e3ecbc1615b84f6e13601f0a8
quickdev <quickdev@a7dca5fb-7094-41a2-a069-28cb1271ccbd>
2008-10-09 09:59:38 +0000

    Followed frameworkd-glib (frameworkd) changes.
    git-svn-id: svn://daria.forty-two.fr/shr/trunk@316 a7dca5fb-7094-41a2-a069-28cb1271ccbd

--- openmoko-messages3/src/messages-main.c
+++ openmoko-messages3/src/messages-main.c
@@ -48,7 +48,7 @@ void connect_to_frameworkd() {
     fwHandler.networkStatus = NULL;
     fwHandler.networkSignalStrength = NULL;
     fwHandler.simAuthStatus = NULL;
-    fwHandler.simIncomingMessage = NULL;
+    fwHandler.simIncomingStoredMessage = NULL;
     fwHandler.callCallStatus = NULL;
     dbus_connect_to_bus(&fwHandler);
 }
