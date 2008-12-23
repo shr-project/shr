4098a96e808f5dda0b48d712f4b9cc66376e5f6d
quickdev <niklas.coding@gmail.com>
2008-12-23 17:46:10 +0100

    Clarified variable naming.

--- libframeworkd-phonegui-efl/src/phonegui-ussd.c
+++ libframeworkd-phonegui-efl/src/phonegui-ussd.c
@@ -4,20 +4,19 @@
 #include "async.h"
 #include "views.h"
 
-static struct Window *win = NULL;
+static struct Window *win_ussd = NULL;
 
 static void _show(GHashTable *options);
-static void _hide(struct Window *win);
+static void _hide();
 static void _delete(void *data, Evas_Object *win, void *event_info);
 
 
 void phonegui_ussd_show(int mode, const char *message) {
     g_debug("phonegui_ussd_show(mode=%d, message=%s)", mode, message);
-    if(win == NULL) {
-        win = window_new("Service Data");
+    if(win_ussd == NULL) {
+        win_ussd = window_new("Service Data");
 
         GHashTable *options = g_hash_table_new(g_str_hash, g_str_equal);
-        g_hash_table_insert(options, "win", win);
         g_hash_table_insert(options, "mode", mode);
         g_hash_table_insert(options, "message", message);
         async_trigger(_show, options);
@@ -26,30 +25,26 @@ void phonegui_ussd_show(int mode, const char *message) {
 
 void phonegui_ussd_hide() {
     g_debug("phonegui_ussd_hide()");
-    if(win != NULL) {
-        async_trigger(_hide, win);
-    }
+    async_trigger(_hide, NULL);
 }
 
 
 static void _show(GHashTable *options) {
     g_debug("_show()");
-    struct Window *win = g_hash_table_lookup(options, "win");
-    assert(win != NULL);
-
-    window_init(win);
-    window_delete_callback_set(win, _delete);
-    window_view_show(win, options, ussd_view_show, ussd_view_hide);
+    assert(win_ussd != NULL);
+    window_init(win_ussd);
+    window_delete_callback_set(win_ussd, _delete);
+    window_view_show(win_ussd, options, ussd_view_show, ussd_view_hide);
 }
 
-static void _hide(struct Window *win) {
+static void _hide() {
     g_debug("_hide()");
-    window_destroy(win, NULL);
-    win = NULL;
+    window_destroy(win_ussd, NULL);
+    win_ussd = NULL;
 }
 
 static void _delete(void *data, Evas_Object *win, void *event_info) {
     g_debug("_delete()");
-    win = NULL;
+    win_ussd = NULL;
 }
 
