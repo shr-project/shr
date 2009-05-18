e3d76b5685e2b90faf54f58d4a9fb9ac9beb17ec
Julien 'Ainulindale' Cassignol <ainulindale@gmail.com>
2009-05-18 12:55:19 +0200

    frameworkd-phonegui: Inserted user configurable home code & prefix. Used glib to manage configuration. Will hopefully not break that much.

--- libframeworkd-phonegui/src/frameworkd-phonegui.c
+++ libframeworkd-phonegui/src/frameworkd-phonegui.c
@@ -1,6 +1,7 @@
 /*
  *  Copyright (C) 2008
  *      Authors (alphabetical) :
+ *              Julien "Ainulindalë" Cassignol
  *              quickdev
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -22,48 +23,64 @@
 
 static void *phonegui_library = NULL;
 
+typedef struct
+{
+    gchar *library;
+    gchar *home_code;
+    gchar *home_prefix;
+} Settings;
+
 void phonegui_load(const char *application_name) {
+    Settings *configuration;
+    GKeyFile *keyfile;
+    GKeyFileFlags flags;
+    GError *error = NULL;
 
-    /*
-     * application_name will be used later
-     * in order to define different phoneguis
-     * for different applications
-     */
-
-    char name[64];
-    FILE *f;
-
-    /* Reading gui configuraton file */
-    f = fopen(FRAMEWORKD_PHONEGUI_CONFIG, "r");
-    if(f == NULL) {
-        g_error("Could not open %s", FRAMEWORKD_PHONEGUI_CONFIG);
+    keyfile = g_key_file_new ();
+    flags = G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS;
+    if (!g_key_file_load_from_file (keyfile, FRAMEWORKD_PHONEGUI_CONFIG, flags, &error))
+    {
+        g_error (error->message);
+        return -1;
     }
-    if(fgets(name, 63, f) == NULL) {
-        g_error("Reading failed");
-    }
-    name[strlen(name) - 1] = '\0';
-    fclose(f);
-    
+
+    conf = g_slice_new (Settings);
+    conf->library = g_key_file_get_string(keyfile,"phonegui","library",NULL);
+    conf->home_code = g_key_file_get_string(keyfile,"local","home_code",NULL);
+    conf->home_prefix = g_key_file_get_string(keyfile,"local","home_prefix",NULL);
+
+
 
     /* Load library */
-    phonegui_library = dlopen(name, RTLD_LOCAL | RTLD_LAZY);
-    if(!phonegui_library) {
+    if(conf->library != NULL) {
+        phonegui_library = dlopen(conf->library, RTLD_LOCAL | RTLD_LAZY);
+        if(!phonegui_library) {
+            g_error("Loading %s failed: %s", name, dlerror());
+        } 
+    } else {
         g_error("Loading %s failed: %s", name, dlerror());
     }
 }
 
 
-void *phonegui_get_function(const char *name) {
-    if(phonegui_library == NULL)
-    {
-        g_error("phonegui library not loaded");
+    void *phonegui_get_function(const char *name) {
+        if(phonegui_library == NULL)
+        {
+            g_error("phonegui library not loaded");
+        }
+
+        void *pointer = dlsym(phonegui_library, name);
+        char *error;
+        if((error = dlerror()) != NULL)  {
+            g_error("Symbol not found: %s", error);
+        }
+        return pointer;
     }
 
-    void *pointer = dlsym(phonegui_library, name);
-    char *error;
-    if((error = dlerror()) != NULL)  {
-        g_error("Symbol not found: %s", error);
-    }
-    return pointer;
+gchar* phonegui_get_user_home_code() {
+    return conf->home_code;
 }
 
+gchar* phonegui_get_user_home_prefix() {
+    return conf->home_prefix;
+}
