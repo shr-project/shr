a0a962e92eacd7c445952180de9d49013bd5513e
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-06-18 17:28:55 +0200

    shr-splash: run in background and be able to use compressed theme images

--- shr-splash/shr-splash.sh
+++ shr-splash/shr-splash.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 case "$1" in
     start)
-       [ -f /usr/share/shr-splash/theme/boot.fb ] && cat /usr/share/shr-splash/theme/boot.fb > /dev/fb0
+       if [ -f /usr/share/shr-splash/theme/boot.fb ]; then
+         (cat /usr/share/shr-splash/theme/boot.fb > /dev/fb0) &
+       else
+         ([ -f /usr/share/shr-splash/theme/boot.fbz ] && zcat /usr/share/shr-splash/theme/boot.fbz > /dev/fb0) &
+       fi
     ;;
     stop)
-       [ -f /usr/share/shr-splash/theme/shutdown.fb ] && cat /usr/share/shr-splash/theme/shutdown.fb > /dev/fb0
+       if [ -f /usr/share/shr-splash/theme/shutdown.fb ]; then
+         (cat /usr/share/shr-splash/theme/shutdown.fb > /dev/fb0) &
+       else
+         ([ -f /usr/share/shr-splash/theme/shutdown.fbz ] && zcat /usr/share/shr-splash/theme/shutdown.fbz > /dev/fb0) &
+       fi
     ;;
 esac
 
