c3d97171723d8e824cb5acf58b711aa78b3b82d9
Sebastian Krzyszkowiak <seba.dos1@gmail.com>
2009-08-07 20:55:04 +0200

    shr-splash: use bzip2 instead of gzip

--- shr-splash/shr-splash.sh
+++ shr-splash/shr-splash.sh
@@ -4,14 +4,14 @@ case "$1" in
        if [ -f /usr/share/shr-splash/theme/boot.fb ]; then
          (cat /usr/share/shr-splash/theme/boot.fb > /dev/fb0) &
        else
-         ([ -f /usr/share/shr-splash/theme/boot.fbz ] && zcat /usr/share/shr-splash/theme/boot.fbz > /dev/fb0) &
+         ([ -f /usr/share/shr-splash/theme/boot.fbz ] && bzcat /usr/share/shr-splash/theme/boot.fbz > /dev/fb0) &
        fi
     ;;
     stop)
        if [ -f /usr/share/shr-splash/theme/shutdown.fb ]; then
          (cat /usr/share/shr-splash/theme/shutdown.fb > /dev/fb0) &
        else
-         ([ -f /usr/share/shr-splash/theme/shutdown.fbz ] && zcat /usr/share/shr-splash/theme/shutdown.fbz > /dev/fb0) &
+         ([ -f /usr/share/shr-splash/theme/shutdown.fbz ] && bzcat /usr/share/shr-splash/theme/shutdown.fbz > /dev/fb0) &
        fi
     ;;
 esac
