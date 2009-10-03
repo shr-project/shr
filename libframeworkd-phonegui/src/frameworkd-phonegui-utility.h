caeef60c930c3c65615c5413f8150bf19f4c9b29
Tom "TAsn" Hacohen <tom@nospam>
2009-10-04 00:02:27 +0200

    split phonegui.c to another file so utility functions will be split

--- libframeworkd-phonegui/src/frameworkd-phonegui-utility.h
+++ libframeworkd-phonegui/src/frameworkd-phonegui-utility.h
@@ -1,5 +1,7 @@
 #ifndef _FRAMEWORKD_PHONEGUI_UTILITY_H
 #define _FRAMEWORKD_PHONEGUI_UTILITY_H
+#include <glib.h>
+
 typedef enum {
     PHONEGUI_DIALOG_ERROR_DO_NOT_USE,  
         // This value is used for checking if we get a wrong pointer out of a HashTable. 
@@ -7,6 +9,12 @@ typedef enum {
     PHONEGUI_DIALOG_MESSAGE_STORAGE_FULL,
     PHONEGUI_DIALOG_SIM_NOT_PRESENT
 } PhoneguiDialogType;
+
+typedef struct
+{
+    gchar *library;
+    GHashTable *contact_cache;
+} Settings;
 
 gchar* phonegui_get_user_home_prefix();
 gchar* phonegui_get_user_home_code();
