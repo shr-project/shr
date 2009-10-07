d128ef89719efa747ccdd3f917fa0ec971757a3d
Tom Hacohen <tom@nospam>
2009-10-08 00:52:02 +0200

    fixed some style issues

--- libframeworkd-phonegui/src/frameworkd-phonegui-utility.h
+++ libframeworkd-phonegui/src/frameworkd-phonegui-utility.h
@@ -2,14 +2,6 @@
 #define _FRAMEWORKD_PHONEGUI_UTILITY_H
 #include <glib.h>
 
-typedef enum {
-	PHONEGUI_DIALOG_ERROR_DO_NOT_USE,
-	// This value is used for checking if we get a wrong pointer out of a HashTable. 
-	// So do not use it, and leave it first in this enum. ( because 0 == NULL )
-	PHONEGUI_DIALOG_MESSAGE_STORAGE_FULL,
-	PHONEGUI_DIALOG_SIM_NOT_PRESENT
-} PhoneguiDialogType;
-
 typedef struct {
 	gchar *library;
 	GHashTable *contact_cache;
@@ -17,12 +9,16 @@ typedef struct {
 
 gchar *phonegui_get_user_home_prefix();
 gchar *phonegui_get_user_home_code();
+/* soon to be deleted functions */
 char *phonegui_contact_cache_lookup(char *number);
+void phonegui_init_contacts_cache();
+void phonegui_destroy_contacts_cache();
+/* end of soon to be deleted */
+
+
 void phonegui_contact_lookup(const char *number,
 			     void (*name_callback) (GError *, char *, gpointer),
 			     void *data);
-void phonegui_init_contacts_cache();
-void phonegui_destroy_contacts_cache();
 int phonegui_sms_send(const char *message, GPtrArray * recipients,
 		void *callback1, void *callback2);
 int phonegui_call_initiate(const char *number,
