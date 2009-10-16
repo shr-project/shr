a3ed3104fd4e2e299c18b1d565aa0334d60d4fa7
Klaus Kurzmann <mok@fluxnetz.de>
2009-10-16 20:13:30 +0200

    ophonekitd: do not segfault when there is no phoneuid running
    Signed-off-by: Klaus Kurzmann <mok@fluxnetz.de>

--- ophonekitd/src/ophonekitd/ophonekitd-phoneuid-dbus.c
+++ ophonekitd/src/ophonekitd/ophonekitd-phoneuid-dbus.c
@@ -24,111 +24,125 @@ _dbus_error(GError *error)
 	g_error_free(error);
 }
 
+static DBusGProxy *
+_dbus_get_proxy(const char *name, const char *path, const char *interface)
+{
+	if (!session_bus) {
+		g_warning("no UI handler registered with us :(");
+		return (NULL);
+	}
+
+	DBusGProxy *proxy = dbus_g_proxy_new_for_name(session_bus,
+			name, path, interface);
+
+	return (proxy);
+}
+
 /* --- org.shr.phoneuid.CallManagement --- */
 void phoneuid_call_management_show_incoming(int callid, int status,
 		const char *number)
 {
-	GError *error = NULL;
-	DBusGProxy *proxy = dbus_g_proxy_new_for_name(session_bus,
-			PHONEUID_SERVICE,
+	DBusGProxy *proxy = _dbus_get_proxy
+			(PHONEUID_CALL_MANAGEMENT_NAME,
 			PHONEUID_CALL_MANAGEMENT_PATH,
 			PHONEUID_CALL_MANAGEMENT_INTERFACE);
-	if (proxy && error == NULL) {
+	if (proxy) {
+		GError *error = NULL;
 		dbus_g_proxy_call (proxy, "DisplayIncoming", &error,
 			G_TYPE_INT, callid, G_TYPE_INT, status,
 			G_TYPE_STRING, number, G_TYPE_INVALID,
 			G_TYPE_INVALID);
+		if (error)
+			_dbus_error(error);
 	}
-	if (error)
-		_dbus_error(error);
 }
 
 void phoneuid_call_management_hide_incoming(int callid)
 {
-	GError *error = NULL;
-	DBusGProxy *proxy = dbus_g_proxy_new_for_name(session_bus,
-			PHONEUID_SERVICE,
+	DBusGProxy *proxy = _dbus_get_proxy
+			(PHONEUID_CALL_MANAGEMENT_NAME,
 			PHONEUID_CALL_MANAGEMENT_PATH,
 			PHONEUID_CALL_MANAGEMENT_INTERFACE);
-	if (proxy && error == NULL) {
+	if (proxy) {
+		GError *error = NULL;
 		dbus_g_proxy_call (proxy, "HideIncoming", &error,
 			G_TYPE_INT, callid, G_TYPE_INVALID,
 			G_TYPE_INVALID);
+		if (error)
+			_dbus_error(error);
 	}
-	if (error)
-		_dbus_error(error);
 }
 
 void
 phoneuid_call_management_show_outgoing(int callid, int status,
 		const char *number)
 {
-	GError *error = NULL;
-	DBusGProxy *proxy = dbus_g_proxy_new_for_name(session_bus,
-			PHONEUID_SERVICE,
+	DBusGProxy *proxy = _dbus_get_proxy
+			(PHONEUID_CALL_MANAGEMENT_NAME,
 			PHONEUID_CALL_MANAGEMENT_PATH,
 			PHONEUID_CALL_MANAGEMENT_INTERFACE);
-	if (proxy && error == NULL) {
+	if (proxy) {
+		GError *error = NULL;
 		dbus_g_proxy_call (proxy, "DisplayOutgoing", &error,
 			G_TYPE_INT, callid, G_TYPE_INT, status,
 			G_TYPE_STRING, number, G_TYPE_INVALID,
 			G_TYPE_INVALID);
+		if (error)
+			_dbus_error(error);
 	}
-	if (error)
-		_dbus_error(error);
 }
 
 void
 phoneuid_call_management_hide_outgoing(int callid)
 {
-	GError *error = NULL;
 	DBusGProxy *proxy = dbus_g_proxy_new_for_name(session_bus,
-			PHONEUID_SERVICE,
+			PHONEUID_CALL_MANAGEMENT_NAME,
 			PHONEUID_CALL_MANAGEMENT_PATH,
 			PHONEUID_CALL_MANAGEMENT_INTERFACE);
-	if (proxy && error == NULL) {
+	if (proxy) {
+		GError *error = NULL;
 		dbus_g_proxy_call (proxy, "HideOutgoing", &error,
 			G_TYPE_INT, callid, G_TYPE_INVALID,
 			G_TYPE_INVALID);
+		if (error)
+			_dbus_error(error);
 	}
-	if (error)
-		_dbus_error(error);
 }
 
 /* --- org.shr.phoneuid.Messages --- */
 void
 phoneuid_messages_display_item(const char *message_path)
 {
-	GError *error = NULL;
-	DBusGProxy *proxy = dbus_g_proxy_new_for_name(session_bus,
-			PHONEUID_SERVICE,
+	DBusGProxy *proxy = _dbus_get_proxy
+			(PHONEUID_MESSAGES_NAME,
 			PHONEUID_MESSAGES_PATH,
 			PHONEUID_MESSAGES_INTERFACE);
-	if (proxy && error == NULL) {
+	if (proxy) {
+		GError *error = NULL;
 		dbus_g_proxy_call (proxy, "DisplayItem", &error,
 			G_TYPE_STRING, message_path, G_TYPE_INVALID,
 			G_TYPE_INVALID);
+		if (error)
+			_dbus_error(error);
 	}
-	if (error)
-		_dbus_error(error);
 }
 
 /* --- org.shr.phoneuid.Dialogs --- */
 void
 phoneuid_dialogs_show_dialog(int dialog)
 {
-	GError *error = NULL;
-	DBusGProxy *proxy = dbus_g_proxy_new_for_name(session_bus,
-			PHONEUID_SERVICE,
+	DBusGProxy *proxy = _dbus_get_proxy
+			(PHONEUID_DIALOGS_NAME,
 			PHONEUID_DIALOGS_PATH,
 			PHONEUID_DIALOGS_INTERFACE);
-	if (proxy && error == NULL) {
+	if (proxy) {
+		GError *error = NULL;
 		dbus_g_proxy_call(proxy, "DisplayDialog", &error,
 				G_TYPE_INT, dialog, G_TYPE_INVALID,
 				G_TYPE_INVALID);
+		if (error)
+			_dbus_error(error);
 	}
-	if (error)
-		_dbus_error(error);
 }
 
 void
